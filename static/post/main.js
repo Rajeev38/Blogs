cb3d0870ec23ad6aea747b36e77f4c46692f7b21
Rajeev Kushwaha <rajeevkushwaha38@gmail.com>
2020-07-03 10:01:36 +0530

    Changes to static files

---
+++ static/post/main.js
@@ -0,0 +1,8 @@
+function addComment(id) {
+   document.getElementByName("b"+id).style.display = "flex";
+}
+
+
+function hideSec(id){
+   document.getElementByName("b"+id).style.display = "none";
+}
